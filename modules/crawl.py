9295e5f5bad5f5beb4a8821d175ac7436b7ba8c3
Micah-T <45294130+Micah-T@users.noreply.github.com>
2022-07-26 13:03:12 -0400

    imroved logging and added html fetching

--- modules/crawl.py
+++ modules/crawl.py
@@ -7,22 +7,27 @@ import datetime
 
 def request(s):
     t = r.get(s)
+
     log = open("requestlog.txt", "a")
     log.write(str(s) + "," + str(t.status_code) + "," + str(datetime.datetime.now()) + "\n")
+    log.close()
+
     if t.status_code == 200:
-        print(f"successfully requested {t}")
+        print(f"successfully requested {s}")
         return t
 
     else:
-        print(f"error requesting {t}: {t.status_code}")
+        print(f"error requesting {s}: {t.status_code}")
         return False
 
 def getXML(s):
     response = request(s)
+    if not response:
+        exit()
     if not response.encoding:
         encoding = "utf-8"
     else:
-        encoding = t.encoding
+        encoding = response.encoding
     xml = BeautifulSoup(response.content, 'lxml-xml', from_encoding=encoding)
     return xml
 
@@ -57,8 +62,28 @@ def getURLs(x):
         o.append(url.findNext("loc").text)
     return o
 
+def getHTML(s):
+    response = request(s)
+    if response:
+        if not response.encoding:
+            encoding = "utf-8"
+        else:
+            encoding = response.encoding
+        html = BeautifulSoup(response.content, 'lxml-html', from_encoding=encoding)
+        return html
 
+def HTMLcorpus(s):
+    x = getXML(s)
+    list = getURLs(x)
+    html = []
+    for l in list:
+        h = getHTML(l)
+        html.append(h)
+    return html
 
 def crawl(s):
-    x = getXML(s)
-    print(getURLs(x))
+    html = HTMLcorpus(s)
+    corpus = ""
+    for h in html:
+        x = 1
+    return corpus
