1e93d30dfeb36a145f12651d1dfcfd87ed97f046
Micah-T <45294130+Micah-T@users.noreply.github.com>
2022-07-26 12:17:28 -0400

    added logging

--- modules/crawl.py
+++ modules/crawl.py
@@ -1,18 +1,21 @@
 from bs4 import BeautifulSoup
 import requests as r
 import lxml as lxml
+import datetime
 
 # Somewhat heavily reliant on https://practicaldatascience.co.uk/data-science/how-to-parse-xml-sitemaps-using-python
 
 def request(s):
     t = r.get(s)
+    log = open("requestlog.txt", "a")
+    log.write(str(s) + "," + str(t.status_code) + "," + str(datetime.datetime.now()) + "\n")
     if t.status_code == 200:
         print(f"successfully requested {t}")
         return t
 
     else:
         print(f"error requesting {t}: {t.status_code}")
-        exit()
+        return False
 
 def getXML(s):
     response = request(s)
@@ -22,6 +25,8 @@ def getXML(s):
         encoding = t.encoding
     xml = BeautifulSoup(response.content, 'lxml-xml', from_encoding=encoding)
     return xml
+
+# add capability for RSS and ATOM feeds
 
 def sitemapType(x):
     sitemapindex = x.find_all("sitemapindex")
@@ -34,7 +39,7 @@ def sitemapType(x):
         print("Unrecognized sort of sitemap.")
         return False
 
-# ok so none of my sitemaps have child sitemaps, but just in case
+# ok so none of my sitemaps have child sitemaps, but just in case. Integrate it if you want more functionality.
 
 def getChildSitemaps(x):
     sitemaps = x.find_all("sitemap")
