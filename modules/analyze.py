2dadad5d7c8311fb7cd144cd53904082458d70a2
Micah-T <45294130+Micah-T@users.noreply.github.com>
2022-08-01 11:07:25 -0400

    commented a bunch of stuff out in analysis

--- modules/analyze.py
+++ modules/analyze.py
@@ -1,15 +1,15 @@
-import json
 import fkscore
 
 def analyze(content):
   report = ""
-#  text = "" 
+# The BeautifulSoup output doesn't need this.
+#   text = "" 
 #  for line in content:
 #    text = text + line.replace("\n", " ")
       
   f = fkscore.fkscore(content)
     
-  report = report + "# Reading Ease Report for " + p["filename"] + "\n"
+ # report = report + "# Reading Ease Report for " + p["filename"] + "\n"
   report = report + "Number of sentences: " + str(f.stats["num_sentences"]) + "\n"
   report = report + "Number of words: " + str(f.stats["num_words"]) + "\n"
   report = report + "Flesch Kincaid reading ease: " + str(f.score["readability"]) + "\n"
