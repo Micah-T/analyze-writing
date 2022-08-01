c3ba072a1a77c75d5230e584554c189a31b153e4
Micah-T <45294130+Micah-T@users.noreply.github.com>
2022-08-01 16:14:22 -0400

    analysis script

--- modules/analyze.py
+++ modules/analyze.py
@@ -1,6 +1,7 @@
 import fkscore
+import json
 
-def analyze(content):
+def analyze(content, sitename):
   report = ""
 # The BeautifulSoup output doesn't need this.
 #   text = "" 
@@ -8,15 +9,30 @@ def analyze(content):
 #    text = text + line.replace("\n", " ")
       
   f = fkscore.fkscore(content)
-    
- # report = report + "# Reading Ease Report for " + p["filename"] + "\n"
+  
+  fkdata = {
+    "sitename": sitename,
+    "number of sentences": str(f.stats["num_sentences"]),
+    "number of words": str(f.stats["num_words"]),
+    "Flesch Kincaid reading ease": str(f.score["readability"]),
+    "Flesch Kincaid grade level": str(f.score["grade"])
+  }
+  
+  report = report + "# Reading Ease Report for " + sitename + "\n"
   report = report + "Number of sentences: " + str(f.stats["num_sentences"]) + "\n"
   report = report + "Number of words: " + str(f.stats["num_words"]) + "\n"
   report = report + "Flesch Kincaid reading ease: " + str(f.score["readability"]) + "\n"
-  report = report + "Flesh Kincaid grade level: " + str(f.score["grade"]) + "\n\n"
+  report = report + "Flesch Kincaid grade level: " + str(f.score["grade"]) + "\n\n"
 
-  stats = open("stats.txt", "w")
+  stats = open(f"_output/{sitename}-stats.txt", "w")
   stats.write(str(report))
   stats.close()
+  print(f"Report for {sitename} written to _output/{sitename}-stats.txt.")
 
-  print("Report written to stats.txt.")
+  stats = open(f"_output/{sitename}-stats.json", "w")
+  jsonreport = json.dumps(fkdata, indent=4)
+  stats.write(jsonreport)
+  stats.close()
+  print(f"Report for {sitename} written to _output/{sitename}-stats.json.")
+
+  return fkdata
