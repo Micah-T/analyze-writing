a7b789559bec63967db4ce1390ba949473c3d4a2
Micah-T <45294130+Micah-T@users.noreply.github.com>
2022-08-01 16:14:07 -0400

    connected analysis script

--- index.py
+++ index.py
@@ -3,9 +3,9 @@ import modules.crawl as crawl
 import modules.analyze as analyze
 import modules.generateHTML as generateHTML
 
-def process(s):
+def process(s, n):
     t = crawl.crawl(s)
-    a = analyze.analyze(t)
+    a = analyze.analyze(t, n)
 #     g = generateHTML.generateHTML(a)
 
 f = open("sites.json", "r")
@@ -15,7 +15,7 @@ print(sites)
 
 for s in sites["sites"]:
     print(f"{s['sitename']}--{s['feedurl']}")
-    process(s["feedurl"])
+    process(s["feedurl"], s["sitename"])
     print(crawl.errorpages)
 
 f.close()
